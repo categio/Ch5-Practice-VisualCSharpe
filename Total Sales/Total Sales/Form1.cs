1c0a3668375b3557f2ef39c11dc73b6e8cc6db74
Categio <categio1@gmail.com>
2022-02-27 18:22:44 -0800

    Updated a total sales calculator to take data from a text document and add to the total and print that total to a label.

---
+++ Total Sales/Total Sales/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Total_Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declare variables to hold val for sales amt and the total sales
+                decimal sales, total = 0m;
+
+                //declare StreamReader variable
+                StreamReader inputFile;
+
+                //Open & create inputFile object
+                inputFile = File.OpenText("Sales.txt");
+
+                //read contents of Sales.txt
+                while (!inputFile.EndOfStream)
+                {
+                    //get sales amount
+                    sales = decimal.Parse(inputFile.ReadLine());
+
+                    //add to the total
+                    total += sales;
+                }
+
+                //close StreamReader file
+                inputFile.Close();
+
+                //Display total
+                totalLabel.Text = total.ToString("C");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
