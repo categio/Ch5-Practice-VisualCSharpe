d925fb8b2d5a23d59046ce87ca2f99debfb6dac1
Categio <categio1@gmail.com>
2022-02-27 18:46:05 -0800

    created a rudimentary coin toss application. works, if number is even in range of 0-7, tails. otherwise, heads.

--- Coin Toss/Coin Toss/Form1.Designer.cs
+++ Coin Toss/Coin Toss/Form1.Designer.cs
@@ -29,13 +29,94 @@ namespace Coin_Toss
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tailsPictureBox = new System.Windows.Forms.PictureBox();
+            this.headsPictureBox = new System.Windows.Forms.PictureBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.coinTossLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tailsPictureBox
+            // 
+            this.tailsPictureBox.Image = global::Coin_Toss.Properties.Resources.Tails1;
+            this.tailsPictureBox.Location = new System.Drawing.Point(38, 75);
+            this.tailsPictureBox.Name = "tailsPictureBox";
+            this.tailsPictureBox.Size = new System.Drawing.Size(162, 181);
+            this.tailsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tailsPictureBox.TabIndex = 1;
+            this.tailsPictureBox.TabStop = false;
+            // 
+            // headsPictureBox
+            // 
+            this.headsPictureBox.Image = global::Coin_Toss.Properties.Resources.Heads1;
+            this.headsPictureBox.Location = new System.Drawing.Point(40, 75);
+            this.headsPictureBox.Name = "headsPictureBox";
+            this.headsPictureBox.Size = new System.Drawing.Size(162, 181);
+            this.headsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.headsPictureBox.TabIndex = 0;
+            this.headsPictureBox.TabStop = false;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Location = new System.Drawing.Point(24, 295);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(86, 44);
+            this.tossButton.TabIndex = 2;
+            this.tossButton.Text = "&Toss Coin";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(133, 295);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(86, 44);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // coinTossLabel
+            // 
+            this.coinTossLabel.AutoSize = true;
+            this.coinTossLabel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.coinTossLabel.Location = new System.Drawing.Point(78, 24);
+            this.coinTossLabel.Name = "coinTossLabel";
+            this.coinTossLabel.Size = new System.Drawing.Size(89, 18);
+            this.coinTossLabel.TabIndex = 4;
+            this.coinTossLabel.Text = "Flip a Coin!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(241, 359);
+            this.Controls.Add(this.coinTossLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.headsPictureBox);
+            this.Controls.Add(this.tailsPictureBox);
+            this.Font = new System.Drawing.Font("Eras Demi ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Coin Toss";
+            ((System.ComponentModel.ISupportInitialize)(this.tailsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox headsPictureBox;
+        private System.Windows.Forms.PictureBox tailsPictureBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label coinTossLabel;
     }
 }
 
--- Coin Toss/Coin Toss/Form1.cs
+++ Coin Toss/Coin Toss/Form1.cs
@@ -16,5 +16,40 @@ namespace Coin_Toss
         {
             InitializeComponent();
         }
+
+        private void tossButton_Click(object sender, EventArgs e)
+        {
+            //holds value of coin side up
+            int sideUp;
+
+            //create Random obj
+            Random rand = new Random();
+
+            //get random int in range of 0-7
+            //Even = tails
+            //Odd = heads
+            sideUp = rand.Next(7);
+
+            //display side that should be up
+            if (sideUp == 0 || sideUp == 2 || sideUp == 4 || sideUp == 6)
+            {
+                //display tails
+                tailsPictureBox.Visible = true;
+                headsPictureBox.Visible = false;
+            }
+            else
+            {
+                //heads
+                headsPictureBox.Visible = true;
+                tailsPictureBox.Visible = false;
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form/program
+            this.Close();
+        }
     }
 }
