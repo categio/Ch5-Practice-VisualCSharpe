ecabbda2bcf5a38a255faefc885a8d0bb0f9973c
Categio <categio1@gmail.com>
2022-02-27 16:47:06 -0800

    Create Friend List project. Project writes names to a .txt file, can print the names to a message box.

---
+++ Friend File/Friend File/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Friend_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.writeNameButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.printButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(273, 57);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // writeNameButton
+            // 
+            this.writeNameButton.Location = new System.Drawing.Point(17, 57);
+            this.writeNameButton.Name = "writeNameButton";
+            this.writeNameButton.Size = new System.Drawing.Size(75, 23);
+            this.writeNameButton.TabIndex = 6;
+            this.writeNameButton.Text = "&Write Name";
+            this.writeNameButton.UseVisualStyleBackColor = true;
+            this.writeNameButton.Click += new System.EventHandler(this.writeNameButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(161, 17);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(153, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Location = new System.Drawing.Point(46, 20);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(109, 13);
+            this.promptLabel.TabIndex = 4;
+            this.promptLabel.Text = "Enter a friend\'s name:";
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(102, 57);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(75, 23);
+            this.printButton.TabIndex = 8;
+            this.printButton.Text = "&Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(187, 57);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "&Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 92);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.writeNameButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.promptLabel);
+            this.Name = "Form1";
+            this.Text = "Friend File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button writeNameButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Friend File/Friend File/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declare StreamWriter variable
+                StreamWriter outputFile;
+
+                //create a file and create outputFile object
+                outputFile = File.AppendText("Friend.txt");
+
+                //write name to the file
+                outputFile.WriteLine(nameTextBox.Text);
+
+                //close the file
+                outputFile.Close();
+
+                //message to let user know data added to file
+                MessageBox.Show("The name was written to the file.");
+            }
+            catch (Exception ex)
+            {
+                //display an error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declare variable to hold item to read from the file
+                string friendName;
+
+                //declare a StreamReader variable
+                StreamReader inputFile;
+
+                //Open the file and get an inputFile obj
+                inputFile = File.OpenText("Friend.txt");
+
+                //iterate over the document and print each name
+                while (!inputFile.EndOfStream)
+                {
+                    //print to the message box the names
+                    friendName = inputFile.ReadLine();
+                    MessageBox.Show("You have entered: " +friendName+ ". Click OK "+
+                        "to see the next name you entered.");
+                }
+
+                //close the file
+                inputFile.Close();
+
+            }
+            catch (Exception ex)
+            {
+                //display error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+
+    }
+}
